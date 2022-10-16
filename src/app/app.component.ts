54ba0111c4c2cdb1ff1d773d2a0c7b8b4e055fd5
Nelson MOntaño <nelson.montano@gmail.com>
2022-10-15 21:26:27 -0500

    destructuring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-first';
+
+  //https://bobbyhadz.com/blog/typescript-binding-element-implicitly-has-an-any-type
+  //Manejo de destructuring con parámetros opcionales y valores x default.
+  getData({
+    id,
+    name,
+    surname
+  }:{
+    id?:number,
+    name?:string,
+    surname?:string
+  }){
+    id=id || 0
+    name=name || ''
+    surname=surname||''
+
+    console.log("id:" + id.toString() + " name: " + name + " surname: " + surname);
+  }
+
+  // getData($event:any){
+  //   console.log("Hola");
+    
+  // }
+
 }
